8a95576437cdf89f8026a24a0b03ef1d1173db4d
Jorge Cadavid <cadavidjorge10@gmail.com>
2023-05-01 11:26:08 -0500

    guardado 2

--- POO.logic/FixedPriceProduct .cs
+++ POO.logic/FixedPriceProduct .cs
@@ -3,8 +3,32 @@
     public class FixedPriceProduct 
     { 
         private double _price;
-        public double price;
+        public double price { get; set; }
         public FixedPriceProduct()
+        {
+            _price = price;
+        }
+
+        public override string ToString()
+        {
+            
+        }
+
+        public  double calculateDiscount(double price) 
+        {
+            
+        }
+        public double calculateTax(double price) 
+        {
+            
+        }
+
+        public double calculateValueToPay(double price)
+        {
+
+        }
+
+        private double validateGreatherThanZero(double price)
         {
 
         }
